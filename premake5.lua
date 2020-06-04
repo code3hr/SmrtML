5306d35c9d02ac2433ba4ed8824a02815a129f6b
code3hr <chickjoel4@gmail.com>
2020-06-04 10:15:08 +0400

    adding a build system with premake5

---
+++ premake5.lua
@@ -0,0 +1,96 @@
+workspace "SmrtML"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+project "SmrtML"
+    location "SmrtML"
+    kind "SharedLib"
+    language "c++"
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+    includedirs
+    {
+      "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+         cppdialect "c++17"
+         staticruntime "On"
+         systemversion "10.0.18362.0"
+         defines
+         {
+            "SML_PLATFORM_WINDOWS",
+            "SML_BUILD_DLL"
+         }
+         postbuildcommands
+         {
+             ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+         }
+    filter "configurations:Debug"
+        defines "SML_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "SML_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "SML_DIST"
+        optimize "On"    
+    -- filter {"system.windows", "configuration:Release"} for both if needed
+    --   buildoptions "/MT" //FOR MULTI THREADING
+project "Sandbox"
+location "Sandbox"
+kind "ConsoleApp"
+language "c++"
+targetdir("bin/" .. outputdir .. "/%{prj.name}")
+objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+files 
+{
+    "%{prj.name}/src/**.h",
+    "%{prj.name}/src/**.cpp"
+}
+includedirs
+{
+  "SmrtML/vendor/spdlog/include",
+  "SmrtML/src"
+}
+
+links 
+{
+    "SmrtML"
+}
+filter "system:windows"
+     cppdialect "c++17"
+     staticruntime "On"
+     systemversion "10.0.18362.0"
+     defines
+     {
+        "SML_PLATFORM_WINDOWS",
+    
+     }
+    
+filter "configurations:Debug"
+    defines "SML_DEBUG"
+    symbols "On"
+
+filter "configurations:Release"
+defines "SML_RELEASE"
+optimize "On"
+
+filter "configurations:Dist"
+defines "SML_DIST"
+optimize "On"   
